d985691f7341b453d8a316a1482e4fd88658bae2
htoo-mg-thait <htoomaungthait@gmail.com>
2021-06-30 16:57:14 +0630

    lesson 03 complete

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const app = Vue.createApp({
         return {
             product : 'Socks',
             image : './assets/images/socks_blue.jpg',
-            vueMasteryUrl : 'https://www.vuemastery.com/'
+            vueMasteryUrl : 'https://www.vuemastery.com/',
+            invetory : 100,
+            onSale : true
         }
     }
 })
