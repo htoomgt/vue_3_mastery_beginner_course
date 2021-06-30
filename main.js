bbb52f041df01e20fe6e9f7a9c1f8cda9e2f72a9
htoo-mg-thait <htoomaungthait@gmail.com>
2021-06-30 17:22:04 +0630

    lesson 05 commplete

--- main.js
+++ main.js
@@ -8,10 +8,23 @@ const app = Vue.createApp({
             onSale : true,
             details : ['50% cotton', '30% wool', '20% polyester'],
             variants : [
-                {id : 2234, color: 'green'},
-                {id : 2235, color: 'blue'}
+                {id : 2234, color: 'green', image : './assets/images/socks_green.jpg'},
+                {id : 2235, color: 'blue', image : './assets/images/socks_blue.jpg'}
             ],
-            sizes : ['small', 'medium', 'large']
+            sizes : ['small', 'medium', 'large'],
+            cart : 0
         }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1;
+        },
+        updateImage(image){
+            this.image = image;
+        },
+        removeFromCart(){
+            this.cart -= 1;
+        }
+        
     }
 })
