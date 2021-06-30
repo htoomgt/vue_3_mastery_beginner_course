6438fff625b30acf7a1cb16d0a8769ba6bbc9cb1
htoo-mg-thait <htoomaungthait@gmail.com>
2021-06-30 16:04:58 +0630

    lesson 01 complete

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
-    data : function () {
+    data : () =>  {
         return {
-            product : 'Socks'
+            product : 'Socks',
+            description : 'These socks are very good used fine raw materail and produced with good technique.'
         }
     }
 })
