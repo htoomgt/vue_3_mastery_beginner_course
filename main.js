3eb6fa22da73e18b5a57a87246625299a3ba3adf
htoo-mg-thait <htoomaungthait@gmail.com>
2021-06-30 17:08:27 +0630

    lesson 04 complete

--- main.js
+++ main.js
@@ -6,7 +6,12 @@ const app = Vue.createApp({
             vueMasteryUrl : 'https://www.vuemastery.com/',
             invetory : 100,
             onSale : true,
-            details : ['50% cotton', '30% wool', '20% polyester']
+            details : ['50% cotton', '30% wool', '20% polyester'],
+            variants : [
+                {id : 2234, color: 'green'},
+                {id : 2235, color: 'blue'}
+            ],
+            sizes : ['small', 'medium', 'large']
         }
     }
 })
